cadc2e17255b50952259c864fe10fd4d94812472
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-11 22:29:45 -0600

    Solution

--- arraysAndStrings/mergeStringsAternatively/Main.py
+++ arraysAndStrings/mergeStringsAternatively/Main.py
@@ -1,5 +1,10 @@
+from arraysAndStrings.mergeStringsAternatively.Solution import word1, word2, Solution
+
+
 class Main:
-    pass
+    solution = Solution()
+    result = solution.mergeStringsAlternatively(word1, word2)
+    print(result)
 
 
 if __name__ == "__main__":
