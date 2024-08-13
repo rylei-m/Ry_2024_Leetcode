836274bcf1d3708d1c62fa352e94d1eeb51323a4
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-12 22:05:53 -0600

    Solution

--- arraysAndStrings/romanToInteger/Main.py
+++ arraysAndStrings/romanToInteger/Main.py
@@ -1,6 +1,13 @@
+from arraysAndStrings.romanToInteger.Solution import Solution, Test1, Test2, Test3, Test4
+
 
 class Main:
-    pass
+    solution = Solution()
+    t1 = solution.romanToInt(Test1)
+    t2 = solution.romanToInt(Test2)
+    t3 = solution.romanToInt(Test3)
+    t4 = solution.romanToInt(Test4)
+    print(t1, t2, t3, t4)
 
 
 if __name__ == "__Main__":
--- arraysAndStrings/romanToInteger/Solution.py
+++ arraysAndStrings/romanToInteger/Solution.py
@@ -1,3 +1,21 @@
 class Solution:
-    def romanToInt(selfself, s: str) -> int:
-        pass
+    def romanToInt(self, s: str) -> int:
+        numeral = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        su = 0
+        n = len(s)
+        i = 0
+
+        while i < n:
+            if i < n - 1 and numeral[s[i]] < numeral[s[i + 1]]:
+                su += numeral[s[i + 1]] - numeral[s[i]]
+                i += 2
+            else:
+                su += numeral[s[i]]
+                i += 1
+        return su
+
+
+Test1 = "MCMXCIV"
+Test2 = "LVIII"
+Test3 = "XIV"
+Test4 = "XXX"
