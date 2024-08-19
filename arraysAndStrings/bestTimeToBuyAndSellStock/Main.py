839b25b5c11e50f9c9f3228e2e15f98047bcae4f
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-18 22:32:41 -0600

    Setup Stock1

---
+++ arraysAndStrings/bestTimeToBuyAndSellStock/Main.py
@@ -0,0 +1,10 @@
+from arraysAndStrings.bestTimeToBuyAndSellStock.Solution import Solution
+
+
+class Main:
+    solution = Solution()
+    print(solution)
+
+
+if __name__ == "__main__":
+    Main()
